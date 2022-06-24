7a447afedabde2f6cb524b5aa62f4d08a03d839f
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-24 10:11:29 +0530

    Commiting UC2 files

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class FloatNumber
+    {
+        public float FindMaximum(float num1,float num2,float num3)
+        {
+            if(num1.CompareTo(num2)>0 && num1.CompareTo(num3)>0)
+            {
+                return num1;
+            }
+
+            else if(num2.CompareTo(num1)>0 && num2.CompareTo(num3)>0)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 1.2f;
+            float num2 = 2.2f;
+            float num3 = 3.2f;
+
+            FloatNumber floatNumber = new FloatNumber();
+            float max = floatNumber.FindMaximum(num1, num2, num3);
+            Console.WriteLine("Maximum number is " + max);
+        }
+    }
+}
