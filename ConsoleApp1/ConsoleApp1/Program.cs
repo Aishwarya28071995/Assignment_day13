9ec104655a49238e8cc6e0fea1c2baca64313220
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-24 11:55:06 +0530

    Commiting files

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[3];
-            array[0] = 30;
-            array[1] = 20;
-            array[2] = 10;
-
-            MaxNumber maxNumber = new MaxNumber();
-            int maximum = maxNumber.ReturnMaxNumber(array);
-            Console.WriteLine("maximum number is " + maximum);
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            MaximumNumber max = new MaximumNumber();
+            int maxNumber = max.ReturnMaxNumber(num1, num2, num3);
+            Console.WriteLine("max number is " + maxNumber);
 
 
         }
