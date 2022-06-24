89baf8291b6f734d8b6cd6504c8561b57595bf8e
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-24 12:14:12 +0530

    Commiting t.C1.3_1

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -6,48 +6,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-<<<<<<< HEAD
+
     internal class Maximum
     {
        
-        public int FindMaximum(int num1,int num2,int num3)
-=======
-    internal class MaximumNumber
-    {
-
+          
         public int ReturnMaxNumber(int num1,int num2,int num3)
->>>>>>> T.C1.1_1
+
         {
             int temp;
             if(num1.CompareTo(num2)>0 && num1.CompareTo(num3)>0)
             {
-<<<<<<< HEAD
-                temp = num2;
-                num2 = num1;
+
+                temp = num3;
+                num3 = num1;
                 num1 = temp;
-=======
->>>>>>> T.C1.1_1
+
                 return num2;
             }
 
             else if(num2.CompareTo(num1)>0 && num2.CompareTo(num3)>0)
             {
-                return num2;
+                temp = num3;
+                num3 = num1;
+                num1 = temp;
             }
-<<<<<<< HEAD
-=======
 
->>>>>>> T.C1.1_1
-            else
+
+          else
             {
-                temp = num1;
-                num1 = num3;
-                num3 = temp;
-<<<<<<< HEAD
-=======
-
->>>>>>> T.C1.1_1
-                return num2;
+                
+                return num3;
             }
         }
 
